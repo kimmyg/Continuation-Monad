0a89cd474a92519fa921adf8660c9b053fdbd781
Kimball Germane <krgermane@yahoo.com>
2012-03-27 16:44:29 -0600

    Factorial.

--- ContinuationMonad.hs
+++ ContinuationMonad.hs
@@ -11,14 +11,26 @@ instance Monad (Cont k v r) where
 runContinuation :: (t -> r) -> Cont k v r t -> r
 runContinuation k (Cont (c, ms)) = c k
 
---wcm :: Ord k -> k -> v -> Cont r -> Cont r
+--wcm :: Ord k => k -> v -> Cont k v r t -> Cont k v r t
+--wcm k v (Cont (c, ms)) = Cont (c, insert k v ms)
 
+--ccm :: Ord k -> k -> Cont k v r t -> Cont k v [v] t
+--ccm k Cont (c, ms) = 
 
+fact :: Int -> Cont k v r Int
+fact 0 = return 1
+fact n = do
+  acc <- fact (n - 1);
+  return (n * acc)
+
+main = runContinuation print $ fact 5
+
+{-
 main = runContinuation print $ do
   x <- return 5;
   y <- return 6;
   return (x + y)
-
+-}
 {-
 return 5 >>= \x ->
   return 6 >>= \y ->
